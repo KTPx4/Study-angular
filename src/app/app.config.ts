56f5d2e938041b65e8186a94d40a6232a6edd38c
Px4 <px4.vnd@gmail.com>
2025-11-02 09:34:16 +0700

    commit update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter} from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+ 
+import { vi_VN, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import vi from '@angular/common/locales/vi';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
+
+registerLocaleData(vi);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideNzI18n(vi_VN), 
+    importProvidersFrom(FormsModule), 
+    provideAnimationsAsync(),  
+    provideClientHydration(),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,12 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './core/shell/layout/main/main.component';
 import { AuthComponent } from './core/shell/layout/auth/auth.component';
 export const routes: Routes = [
-    // Auth layout routes
+    
     {
         path: 'auth',
         component: AuthComponent,
         loadChildren: () => import('./pages/auth/auth.routes').then(m => m.routes)
     },
-    // Main layout routes
     {
         path: '',
         component: MainComponent,
@@ -30,7 +29,9 @@ export const routes: Routes = [
                 path: 'profile',
                 loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent)
             },
-            { path: ":category", loadComponent: () => import('./pages/product/list-product/list-product.component').then(m => m.ListProductComponent) },
+            { 
+                path: ":category", 
+                loadComponent: () => import('./pages/product/list-product/list-product.component').then(m => m.ListProductComponent) },
             { path: ":category/:slug", loadComponent: () => import('./pages/product/detail-product/detail-product.component').then(m => m.DetailProductComponent) },
             
 
